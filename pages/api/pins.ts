d5f28949d4776981a9115c08d1743140ef17cc58
Paweł Potempa <pwpotempa@gmail.com>
2022-10-05 20:31:32 +0200

    fix: Fix api calls to only return one status

--- pages/api/pins.ts
+++ pages/api/pins.ts
@@ -4,13 +4,15 @@ import { prisma } from "@/lib/prisma";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    const pinsData = JSON.parse(req.body);
-
-    const savedPins = await prisma.pin.create({
-      data: { ...pinsData },
-    });
-
-    res.json(savedPins);
+    try {
+      const pinsData = JSON.parse(req.body);
+      const savedPins = await prisma.pin.create({
+        data: { ...pinsData },
+      });
+      res.status(200).json(savedPins);
+    } catch (err) {
+      res.status(400).json({ message: "Something went wrong" });
+    }
   }
 
   if (req.method === "DELETE") {
@@ -22,5 +24,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     });
   }
 
-  return res.send("Method not allowed");
+  if (req.method !== "POST" && req.method !== "DELETE") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
 };
